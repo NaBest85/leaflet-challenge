e0bca4c307ed67806720ccd79a8bcbd497cd38d0
Nick <nicholasbest85@gmail.com>
2023-09-28 17:59:15 -0400

    Final Commit

--- Leaflet-Part-1/Starter_Code/static/js/logic.js
+++ Leaflet-Part-1/Starter_Code/static/js/logic.js
@@ -1,7 +1,43 @@
-// Define functions for setting marker size based on earthquake magnitude
-let setSize = (magnitude) => magnitude * 15000;
-// Define functions for choosing color based on earthquake depth
-let chooseColor = (depth) => {
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Use D3 to get to get data from API and log it to console
+d3.json(queryUrl).then(function (data) {
+
+    createFeatures(data.features);
+    console.log(data.features);
+});
+
+function createFeatures(earthquakeData) {
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><p><b>Magnitude:</b> ${feature.properties.mag}; <b>Depth:</b> ${feature.geometry.coordinates[2]} Km</p>`);
+    }
+    // Creating a GeoJSON layer that contains the features array on the earthquakeData object.
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function(feature, latlng) {
+            color = markerColor(feature.geometry.coordinates[2]);
+
+            var geoJSONmarkers = {
+                radius: markerSize(feature.properties.mag),
+                fillColor: color,
+                color: "black",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 1
+            };
+            return L.circleMarker(latlng, geoJSONmarkers);
+        }
+    });
+    // Sending earthquakes layer to the createMap function
+    createMap(earthquakes);
+}
+// A function to determine the marker size based on the magnitude of earthquake
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
+
+function markerColor(depth) {
   if (depth <= 10) return "lime";
   else if (depth <= 30) return "green";
   else if (depth <= 50) return "yellow";
@@ -10,68 +46,49 @@ let chooseColor = (depth) => {
   else return "red";
 };
 
-// Create circle markers for each earthquake
-let createMarker = (feature) => {
-let magnitude = feature.properties.mag;
-let depth = feature.geometry.coordinates[2];
-
-  return L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-    color: "black",
-    fillColor: chooseColor(depth),
-    radius: setSize(magnitude),
-    opacity: 1,
-    weight: 1,
-    fillOpacity: 0.5,
-  }).bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-};
-
-// Fetch GeoJSON data and create the map
-let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-d3.json(link).then((data) => {
-  let markers = data.features.map(createMarker);
-  createMap(markers);
-});
-
 // Create the base maps
-let createMap = (markers) => {
+function createMap(earthquakes) {
+  // Creating the base layers.
   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
-
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-
+  // Creating layer groups:for earthquake markers.
+  let earthquake = L.layerGroup(earthquakes);
+  // Creating a baseMaps object.
   let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
+      "Street Map": street
   };
-// set the overlay layer
+  // Creating an overlay object.
   let overlayMaps = {
-    Earthquakes: L.layerGroup(markers)
+      Earthquakes: earthquakes
   };
-// set the div to place map and the coordinates of center/zoom level
-  let map = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 4.5,
-    layers: [street, L.layerGroup(markers)]
+  // Defining a map object.
+  let myMap = L.map('map', {
+      center: [37.09, -95.71],
+      zoom: 5,
+      layers: [street, earthquakes]
   });
-// add layers to map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(map);
-// create the legend to add to map
-  const legend = L.control({ position: "bottomright" });
-  legend.onAdd = () => {
-    const div = L.DomUtil.create("div", "info legend");
-    const depth = [-10, 10, 30, 50, 70, 90];
+  // Set up the legend.
+  let legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+      let div = L.DomUtil.create("div", "info legend")
+      let depth = [-10, 10, 30, 50, 70, 90];
+      let labels = [];
 
-    for (let i = 0; i < depth.length; i++) {
-      div.innerHTML +=
-        `<i style="background:${chooseColor(depth[i] + 1)}"></i> ${depth[i]}${depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+'}`;
-    }
-    return div;
+      div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < depth.length; i++) {
+          div.innerHTML +=
+              '<i style="background-color:' + markerColor(depth[i]+1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+              depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+      }
+
+      return div;
   };
-  legend.addTo(map);
-};
+
+  legend.addTo(myMap);
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+  }).addTo(myMap);
+}
